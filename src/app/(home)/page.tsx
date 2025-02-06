838e739b81deef73296a89ab12e320d66f7a13dd
TusharSachdeva29 <161271923+TusharSachdeva29@users.noreply.github.com>
2025-02-06 11:54:54 +0530

    clerk auth added:

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,20 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 // import { Link } from "lucide-react"
 import Link from "next/link"
 import { Navbar } from "./navbar"
 import { TemplateGallery } from "./template-gallery"
+import { useQuery } from "convex/react"
+import { api } from "../../../convex/_generated/api"
 
 const Home = () => {
+  const documents = useQuery(api.documents.get)
+
+  if(documents === undefined){
+    return <div>Loading...</div>
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -12,10 +22,14 @@ const Home = () => {
       </div>
       <div className="mt-16">
         <TemplateGallery />
+        {documents?.map((document) => (
+          <span key={document._id}>
+            {document.title}
+          </span>
+        ))}
       </div>
     </div>
   )
 }
-
 
 export default Home  

06f0aa8fae6e826c79116b3fec49a9838da214a9
TusharSachdeva29 <161271923+TusharSachdeva29@users.noreply.github.com>
2025-02-09 18:15:06 +0530

    avatar✅:

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -20,6 +20,8 @@ import {
 import { useEditorStore } from "@/store/use-editor-store";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
+import { Avatars } from "./avatars";
+
 export const Navbar = () => {
     const {editor} = useEditorStore()
 
@@ -207,6 +209,7 @@ export const Navbar = () => {
                 </div>
             </div>
             <div className="flex gap-3 items-center pl-6">
+                <Avatars />
                 <OrganizationSwitcher afterCreateOrganizationUrl="/"
                 afterLeaveOrganizationUrl="/"
                 afterSelectOrganizationUrl="/"
